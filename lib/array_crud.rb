26a78b60c1f567b8812b236e4022e65952aca536
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-09-29 18:50:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  an_array=["Tom", "Pam", 5, 6]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  an_array= <<"Lizz"
 end
 
 def add_element_to_start_of_array(array, element)
